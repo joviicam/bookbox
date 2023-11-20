efd78f437da44d17ccf496526a7a9cd45e9ecb7a
joviicam <20213tn110@utez.edu.mx>
2023-11-20 03:13:18 -0600

    IndexScreen con buscador funcional y 2 components

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,9 +1,11 @@
 let colorPrimary = "#2F4858";
 let colorPrimaryDark = "#FFA000";
 let colorAccent = "#FF5722";
+let green = "#77BA99";
 
 export default {
   COLOR_PRIMARY: colorPrimary,
   COLOR_PRIMARY_DARK: colorPrimaryDark,
   COLOR_ACCENT: colorAccent,
+  GREEN: green,
 };

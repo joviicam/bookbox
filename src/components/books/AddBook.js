c491572e36df6dc0cedb4547a323a7cf6bb2a730
MigueDeep <20213tn055@utez.edu.mx>
2023-11-26 22:39:19 -0600

    modificacion en el registro

--- src/components/books/AddBook.js
+++ src/components/books/AddBook.js
@@ -3,15 +3,30 @@ import { StyleSheet, Text, View, ImageBackground} from "react-native";
 import colors from "../../utils/colors";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { Input, Icon, Button } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 export default function AddBook() {
+
+  const navigation = useNavigation();
+
     return (
         <KeyboardAwareScrollView>
             <ImageBackground
             source={require("../../../assets/images/libreria.jpg")}
             style={styles.background}
             >
+              {/* Icono de regresar */}
+            <View style={styles.titleBar}>
+            <Icon
+              type="material-community"
+              name="arrow-left"
+              size={30}
+              color={"white"}
+              containerStyle={styles.btnBack}
+              onPress={() => navigation.goBack()}
+            />
             <Text style={styles.logoTitle}>Añadir libro</Text>
+            </View>
             <View style={styles.containerForm}>
                 
                 <Text style={styles.text}>Nombre</Text>
@@ -26,17 +41,21 @@ export default function AddBook() {
                 placeholder="Autor"
                 placeholderTextColor={colors.PRIMARY_COLOR}
                 />
-                <Text style={styles.text}>ISBN</Text>
-                <Input
-                style={styles.input}
-                placeholder="ISBN"
-                placeholderTextColor={colors.PRIMARY_COLOR}
-                />
                 <Text style={styles.text}>Género</Text> 
                 <Input
                 style={styles.input}
                 placeholder="Género"
                 placeholderTextColor={colors.PRIMARY_COLOR}
+                />
+                <Text style={styles.text}>Descripción</Text>
+                <Input
+                style={styles.input}
+                placeholder="Descripción"
+                placeholderTextColor={colors.PRIMARY_COLOR}
+                />
+                <Button
+                  title={"Añadir libro"}
+                  buttonStyle={styles.btnAddBook}
                 />
             </View>
             </ImageBackground>
@@ -46,19 +65,18 @@ export default function AddBook() {
 }
 
 const styles = StyleSheet.create({
-    containerLogo: {
-        flexDirection: "row",
-        alignItems: "center",
-        marginTop: 150,
+
+      text:{
+        fontWeight: "bold",
+        color: colors.PRIMARY_COLOR,
+        textAlign: "left",
+        alignSelf: "flex-start",
+        fontFamily: "Roboto",
       },
       background: {
         resizeMode: "cover", 
         alignItems: "center",
-        height: 500
-      },
-      image: {
-        width: 70,
-        height: 70,
+        height: 800
       },
       logoTitle: {
         fontSize: 30,
@@ -70,7 +88,7 @@ const styles = StyleSheet.create({
         marginTop: 20,
       },
       containerForm: {
-        height: 400,
+        height: 500,
         width: "80%",
         backgroundColor: "white",
         borderTopLeftRadius: 30,
@@ -78,5 +96,23 @@ const styles = StyleSheet.create({
         borderBottomLeftRadius: 30,
         borderBottomRightRadius: 30,
         padding: 20,
+        display: "flex",
+        alignItems: "center",
+        marginTop: 20,
+      },
+      btnAddBook:{
+        width: 250,
+        height: 60,
+        backgroundColor: colors.COLOR_PRIMARY,
+        borderRadius: 30,
+        justifyContent: "center",
+        alignContent: "center",
+        marginBottom: 20,
+      },
+      titleBar: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 24,
+        marginHorizontal: 16,
       },
 });
